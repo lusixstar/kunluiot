d5e2c7e27a557099a64b73a7838faa728a2b9c4b
cw <653054564@qq.com>
2022-01-07 15:19:39 +0800

    jitpack

--- SDK/consumer-rules.pro
+++ SDK/consumer-rules.pro
@@ -1,4 +1,3 @@
-# com.example.entity是存放实体类的路径
--keep class com.kunluiot.sdk.bean.** {
+-keep class com.kunluiot.sdk.** {
     *;
 }
--- app/src/main/java/com/example/kiotsdk/adapter/device/DeviceProductItemAdapter.kt
+++ app/src/main/java/com/example/kiotsdk/adapter/device/DeviceProductItemAdapter.kt
@@ -9,7 +9,6 @@ import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.example.kiotsdk.R
 import com.kunluiot.sdk.bean.device.DeviceProductsBean
-import com.kunluiot.sdk.thirdlib.ws.websocket.util.LogUtil
 
 
 class DeviceProductItemAdapter(list: MutableList<DeviceProductsBean>) : BaseQuickAdapter<DeviceProductsBean, BaseViewHolder>(R.layout.item_device_product_sub_item, list) {
